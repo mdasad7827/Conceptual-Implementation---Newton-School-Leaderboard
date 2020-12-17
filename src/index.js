7ab0a34fe6dca48f73ef3193e6da4b4b774ab982
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-17 14:15:15 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-const { data } = require('./data')
+const { data } = require("./data");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
+app.get("/topRankings", async (req, res) => {
+  const offset = sanitize(req.query.offset, 0);
+  const limit = sanitize(req.query.limit, 20);
+  const newData = await data.splice(offset, limit);
+  res.send(newData);
+});
 
+const sanitize = (value, defVal) => {
+  if (value === null || value === undefined || isNaN(Number(value))) {
+    return defVal;
+  } else {
+    return Number(value);
+  }
+};
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
