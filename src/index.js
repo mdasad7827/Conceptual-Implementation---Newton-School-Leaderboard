f79bac59de2736b3b228cad696b57db32ed3cd2b
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-17 14:22:14 +0530

    slice

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.get("/topRankings", async (req, res) => {
   const offset = sanitize(req.query.offset, 0);
   const limit = sanitize(req.query.limit, 20);
-  const newData = await data.splice(offset, limit);
+  const newData = await data.slice(offset, limit);
   res.send(newData);
 });
 
